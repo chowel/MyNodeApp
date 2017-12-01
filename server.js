aecca76406ef099713b60f3f762ebb1a08374aea
chenchengwei <1755596120@qq.com>
2017-12-01 15:20:55 +0800

    update server

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route, handle) {
   function onRequest(request, response) {
-    console.log("Request received.");
+    var pathname = url.parse(request.url).pathname;
+    console.log("来至: " + pathname + " 的请求已收到.");
+    
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    response.write(route(handle, pathname));
     response.end();
   }
 
   http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+  console.log("服务器启动...");
 }
 
 exports.start = start;
